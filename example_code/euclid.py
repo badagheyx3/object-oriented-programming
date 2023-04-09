c51a9ccb0b31c4b20e896281783e1e47d3c34504
Izzudin Mohd Azhan <im21@ic.ac.uk>
2023-04-09 16:17:41 +0100

    what the dawg doin

--- example_code/euclid.py
+++
@@ -1,7 +0,0 @@
-def gcd(a, b):
-    """Return the greatest common divisor of a and b using a recursive
-    implementation of Euclid's algorithm."""
-    try:
-        return gcd(b, a % b)
-    except ZeroDivisionError:
-        return a
